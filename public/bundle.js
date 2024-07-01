71fd8ef9913268c361246306035fc88727fbda42
Julio Peña <julio@iMac-de-Julio.local>
2024-07-01 09:23:21 -0500

    cargando productos carrito

--- public/bundle.js
+++ public/bundle.js
@@ -51,49 +51,112 @@ btnDisminuirCantidad.addEventListener('click', (e) => {
 });
 
 // botones para acceder al carrito
-const botonesAbrirCarrito = document.querySelectorAll('[data-accion="abrir-carrito"]');
-const botonesCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-carrito"]');
-const ventanaCarrito = document.getElementById('carrito');
-const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
-const producto = document.getElementById('producto');
+const botonesAbrirCarrito = document.querySelectorAll(
+  '[data-accion="abrir-carrito"]'
+);
+const botonesCerrarCarrito = document.querySelectorAll(
+  '[data-accion="cerrar-carrito"]'
+);
+const ventanaCarrito = document.getElementById("carrito");
+const btnAgregarCarrito = document.getElementById("agregar-al-carrito");
+const producto = document.getElementById("producto");
 const carrito = [];
 
-//funcion externa xq la quiero usar en varias partes
+//funcion externa xq la quiero usar en varias partes. Renderizado del carrito (Calcular productos)
 const renderCarrito = () => {
-    ventanaCarrito.classList.add('carrito--active');
-    
-    console.log(carrito);
+  ventanaCarrito.classList.add("carrito--active");
+
+  const productosAnteriores = ventanaCarrito.querySelectorAll('.carrito__producto');
+  productosAnteriores.forEach((productoAnterior) => {
+    productoAnterior.remove();
+  });
+
+  carrito.forEach((productoCarrito) => {
+    //ruta de la imagen
+    let thumbSrc = producto.querySelectorAll('.producto__thumb-img')[0].src;
+    if (productoCarrito.color === 'rojo'){
+        thumbSrc = './img/thumbs/rojo.jpg';
+    } else if(productoCarrito.color === 'amarillo') {
+        thumbSrc = './img/thumbs/amarillo.jpg';
+    }
+
+    // Se creo una palntilla del codigo HTML
+    const plantillaProducto = `
+            <div class="carrito__producto-info">
+								<img src="${thumbSrc}" alt="" class="carrito__thumb" />
+								<div>
+									<p class="carrito__producto-nombre">
+										<span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
+									</p>
+									<p class="carrito__producto-propiedades">
+										Tamaño:<span>${productoCarrito.tamaño}</span> Color:<span>${productoCarrito.color}</span>
+									</p>
+								</div>
+							</div>
+							<div class="carrito__producto-contenedor-precio">
+								<button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+										/>
+									</svg>
+								</button>
+								<p class="carrito__producto-precio">$500.00</p>
+							</div>
+        `;
+
+    // Se creo un div
+    const itemCarrito = document.createElement("div");
+
+    // Se agrega la clase carrito__producto
+    itemCarrito.classList.add("carrito__producto");
+
+    // Se inserto la plantilla dentro del elemento
+    itemCarrito.innerHTML = plantillaProducto;
+
+    // Agregamos el producto a la ventana carrito
+    ventanaCarrito.querySelector(".carrito__body").appendChild(itemCarrito);
+  });
 };
 
 //Abrir carrito
 botonesAbrirCarrito.forEach((boton) => {
-    boton.addEventListener('click', (e) => {
-        renderCarrito();
-    });
+  boton.addEventListener("click", (e) => {
+    renderCarrito();
+  });
 });
 
 //cerrar carrito
 botonesCerrarCarrito.forEach((boton) => {
-    boton.addEventListener('click', (e) => {
-        ventanaCarrito.classList.remove('carrito--active');
-    });
+  boton.addEventListener("click", (e) => {
+    ventanaCarrito.classList.remove("carrito--active");
+  });
 });
 
 // Agregar producto al carrito
-btnAgregarCarrito.addEventListener('click', (e) => {
-    const id = producto.dataset.productoId;
-    const nombre = producto.querySelector('.producto__nombre').innerText;
-    const cantidad = parseInt(producto.querySelector('#cantidad').value);
-    const color = producto.querySelector('#propiedad-color input:checked').value;
-    const tamaño = producto.querySelector('#propiedad-tamaño input:checked').value;
+btnAgregarCarrito.addEventListener("click", (e) => {
+  const id = producto.dataset.productoId;
+  const nombre = producto.querySelector(".producto__nombre").innerText;
+  const cantidad = parseInt(producto.querySelector("#cantidad").value);
+  const color = producto.querySelector("#propiedad-color input:checked").value;
+  const tamaño = producto.querySelector(
+    "#propiedad-tamaño input:checked"
+  ).value;
 
-    //console.log(id, nombre, cantidad, color, tamaño);
-    carrito.push({
-        id: id, 
-        nombre: nombre, 
-        cantidad: cantidad, 
-        color: color,
-        tamaño: tamaño,
-    });
-        
+  //console.log(id, nombre, cantidad, color, tamaño);
+  carrito.push({
+    id: id,
+    nombre: nombre,
+    cantidad: cantidad,
+    color: color,
+    tamaño: tamaño,
+  });
 });
+
+// Mostrar productos en el carrito
